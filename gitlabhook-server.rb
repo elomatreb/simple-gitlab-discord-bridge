d6201c18488830296cc134e1fc232cee5e4526a2
Ole B <git@bertr.am>
2017-07-04 12:05:45 +0200

    Authenticate requests with gitlab secret

--- gitlabhook-server.rb
+++ gitlabhook-server.rb
@@ -1,4 +1,24 @@
 require "sinatra"
+require "yaml"
+require "digest/sha1"
+
+helpers do
+  def config
+    @config ||= YAML.load(File.read("settings.yaml"))
+  end
+
+  def constant_time_equal(a, b)
+    Digest::SHA1.hexdigest(a) == Digest::SHA1.hexdigest(b) rescue false
+  end
+
+  def gitlab_secret
+    config.dig("gitlab", "secret") || raise("No gitlab secret configured!")
+  end
+end
+
+before do
+  halt 401 unless constant_time_equal(gitlab_secret, request.env["HTTP_X_GITLAB_TOKEN"])
+end
 
 post "/push" do
   
