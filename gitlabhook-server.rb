9889037324a0ca211ca5d0b4f7817cd1ccf6f413
Ole B <git@bertr.am>
2017-07-04 16:33:41 +0200

    Don't nest config keys and validate config on startup

--- gitlabhook-server.rb
+++ gitlabhook-server.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "sinatra"
 require "yaml"
 require "json"
@@ -6,21 +8,29 @@ require "digest/sha1"
 
 require "sinatra/reloader" if development?
 
-helpers do
-  def config
-    @config ||= YAML.load(File.read("settings.yaml"))
-  end
+def config
+  @config ||= YAML.safe_load(File.read("settings.yaml"))
+rescue
+  abort "ERROR: settings.yaml could not be loaded. See settings.yaml.example"
+end
 
+# Validate config
+%w[gitlab_secret discord_webhook_url].each do |key|
+  next if config.key? key
+  abort "ERROR: Configuration is missing '#{key}'. See settings.yaml.example"
+end
+
+helpers do
   def constant_time_equal(a, b)
     Digest::SHA1.hexdigest(a) == Digest::SHA1.hexdigest(b) rescue false
   end
 
   def gitlab_secret
-    config.dig("gitlab", "secret") || raise("No gitlab secret configured!")
+    config["gitlab_secret"]
   end
 
   def discord_url
-    config.dig("discord", "webhook_url") || raise("No Discord URL configured!")
+    config["discord_webhook_url"]
   end
 
   # View helpers
